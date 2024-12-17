07845525ba6f4d5d865f75e6bb07b5662378310f
vanloi <maivloi2003@gmail.com>
2024-12-17 22:37:48 +0700

    update

--- src/index.js
+++ src/index.js
@@ -29,6 +29,6 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
-route(app)
+                route(app)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
